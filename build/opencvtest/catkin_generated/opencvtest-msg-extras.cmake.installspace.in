1f009e889d29ee055c0bdb77e1d66b8c5ed42211
chenway <1099481194@qq.com>
2020-07-13 20:13:53 +0800

    backup

--- build/opencvtest/catkin_generated/opencvtest-msg-extras.cmake.installspace.in
+++
@@ -1,2 +0,0 @@
-set(opencvtest_MESSAGE_FILES "msg/pixel_point0.msg")
-set(opencvtest_SERVICE_FILES "")
--- build/opencvtest/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/chenway/Documents/test/devel/include;/home/chenway/Documents/test/src/opencvtest/include".split(';') if "/home/chenway/Documents/test/devel/include;/home/chenway/Documents/test/src/opencvtest/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "cv_bridge;image_transport;roscpp;rospy;sensor_msgs;std_msgs;message_runtime".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lopencvtest".split(';') if "-lopencvtest" != "" else []
-PROJECT_NAME = "opencvtest"
-PROJECT_SPACE_DIR = "/home/chenway/Documents/test/devel"
-PROJECT_VERSION = "0.0.0"
